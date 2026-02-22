6f81bbe4597c080e2bbace77ef538325554c130d
grish-ka <grishka.krot@gmail.com>
2026-02-22 09:32:51 +0000

    Syscall tests

--- src/apps/test.s
+++ src/apps/test.s
@@ -1,4 +1,4 @@
-/* * IotaOS - test.s (Userland App)
+/* * IotaOS - syscallTest.s
  * Copyright (c) 2026 grish-ka
  * Licensed under the MIT License.
  */
@@ -11,15 +11,19 @@
 .short 0x4249       /* magic: 0x4249 ('I' 'B') */
 .short 1            /* version: 1 */
 .long  16           /* code_offset: 16 bytes (size of this header) */
-.long  11           /* code_size: 11 bytes */
+.long  44           /* code_size: 13 bytes */
 .long  0            /* entry_point: 0 (start immediately after header) */
 
-/* --- ACTUAL PROGRAM CODE --- */
 _start:
-    /* Direct VGA access to prove we ran! 
-       Prints a Yellow '!' at the top left of the screen */
-    movl $0xB8000, %edi
-    movw $0x0E21, (%edi) 
-    
-    /* Return back to the kernel shell */
-    ret
+    call next_line       /* Push current EIP to stack */
+next_line:
+    pop %ebx             /* EBX now knows exactly where we are in RAM */
+    add $(msg - next_line), %ebx /* Offset EBX to point to the string */
+
+    mov $1, %eax         /* Syscall 1: PRINT */
+    int $0x80
+
+    ret                  /* Back to Kernel Shell */
+
+msg:
+    .asciz "IotaOS: Syscall Success!\n"
